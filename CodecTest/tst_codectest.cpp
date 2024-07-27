be5ed4e6a01eaa0d58756d6d8a606cb0ec29d846
JustAnotherAmateur <k.fuyor@gmail.com>
2024-07-27 19:03:20 +0300

    codec tests

--- CodecTest/tst_codectest.cpp
+++ CodecTest/tst_codectest.cpp
@@ -27,18 +27,89 @@ public:
     ~CodecTest() = default;
 
 private slots:
-    void test_encode();
+    void test_data();
+
+    void test();
 };
 
-void CodecTest::test_encode()
+void CodecTest::test_data()
 {
-    QCOMPARE(encode(RawImageData(4, 2,
-                                {
-                                    0xFF, 0xFF, 0xFF, 0xFF,
-                                    0xFF, 0xFF, 0xFF, 0xFF
-                                })),
-             create_vector({4, 2, 1, 1}));
+    QTest::addColumn<RawImageData>("image");
+    QTest::addColumn<RawImageData::PixelContainer>("encoded");
 
+    QTest::newRow("basic_1") << RawImageData(4, 2,
+                                             {
+                                                 0xFF, 0xFF, 0xFF, 0xFF,
+                                                 0xFF, 0xFF, 0xFF, 0xFF
+                                             })
+                             << create_vector({'B', 'A', 4, 2,
+                                               1, 1});
+
+    QTest::newRow("basic_2") << RawImageData(4, 2,
+                                             {
+                                                 0xFF, 0xFF, 0xFF, 0xFF,
+                                                 0x00, 0x00, 0x00, 0x00
+                                             })
+                             << create_vector({'B', 'A', 4, 2,
+                                               1, 0,
+                                               0b10});
+
+    QTest::newRow("basic_3") << RawImageData(4, 2,
+                                             {
+                                                 0xF1, 0xFF, 0xFF, 0xFF,
+                                                 0x00, 0x00, 0x00, 0x00
+                                             })
+                             << create_vector({'B', 'A', 4, 2,
+                                               0, 0,
+                                               0b11, 0xF1, 0xFF, 0xFF, 0xFF,
+                                               0b10});
+
+    QTest::newRow("complex") << RawImageData(8, 4,
+                                             {
+                                                 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+                                                 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                                                 0x00, 0x00, 0x00, 0x00, 0x05, 0x05, 0x06, 0x06,
+                                                 0x07, 0x07, 0x08, 0x08, 0xFF, 0xFF, 0xFF, 0xFF
+                                             })
+                             << create_vector({'B', 'A', 8, 4,
+                                               1, 0, 0, 0,
+                                               0b10, 0b10,
+                                               0b10, 0b11, 0x05, 0x05, 0x06, 0x06,
+                                               0b11, 0x07, 0x07, 0x08, 0x08, 0b00});
+
+    QTest::newRow("irregular_size_1") << RawImageData(1, 1,
+                                                      {
+                                                          0xFF
+                                                      })
+                                      << create_vector({'B', 'A', 1, 1,
+                                                        1});
+
+    QTest::newRow("irregular_size_2") << RawImageData(1, 1,
+                                                      {
+                                                          0x00
+                                                      })
+                                      << create_vector({'B', 'A', 1, 1,
+                                                        0,
+                                                        0b10});
+
+    QTest::newRow("irregular_size_3") << RawImageData(6, 2,
+                                                      {
+                                                          0x05, 0x05, 0x00, 0x00, 0x00, 0x00,
+                                                          0xFF, 0xFF, 0xFF, 0xFF, 0x07, 0x07
+                                                      })
+                                      << create_vector({'B', 'A', 6, 2,
+                                                        0, 0,
+                                                        0b11, 0x05, 0x05, 0x00, 0x00, 0b10,
+                                                        0b00, 0b11, 0x07, 0x07});
+}
+
+void CodecTest::test()
+{
+    QFETCH(RawImageData, image);
+    QFETCH(RawImageData::PixelContainer, encoded);
+
+    QCOMPARE(encode(image), encoded);
+    QCOMPARE(decode(encoded), image);
 }
 
 QTEST_APPLESS_MAIN(CodecTest)
